ca8977d4fdc9061a55d84df178665499ec8401ee
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-02-26 00:47:01 +0600

    feat: add code of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+/* jshint esversion: 6 */
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openingSymbols = [];
+  const closingSymbols = [];
+  for (const arr of bracketsConfig) {
+    openingSymbols.push(arr[0]);
+    closingSymbols.push(arr[1]);
+  }
+  const isOpeningSymbol = (symbol) => openingSymbols.includes(symbol);
+  const getClosingSymbolFor = (symbol) => closingSymbols[openingSymbols.indexOf(symbol)];
+  const stack = [];
+  const indexStack = [];
+  for (const symbol of str) {
+    if (symbol === '|') {
+      indexStack.push(str.indexOf(symbol));
+      continue;
+    }
+    if (isOpeningSymbol(symbol)) {
+      const closingSymbol = getClosingSymbolFor(symbol);
+      stack.push(closingSymbol);
+    } else {
+      const lastSavedSymbol = stack.pop();
+      if (symbol !== lastSavedSymbol) {
+        return false;
+      }
+    }
+  }
+  if (indexStack.length % 2 !== 0) {
+    return false;
+  }
+
+ 
+  return stack.length === 0;
+
+};
+
